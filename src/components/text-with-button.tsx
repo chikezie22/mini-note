9d634e09174dc5de6170493be386ebd5a4d93217
chikezie22 <simonchikezie44@gmail.com>
2025-04-05 18:04:11 +0100

    refactor: enhance TextareaWithButton component with state management and submission logic

--- src/components/text-with-button.tsx
+++ src/components/text-with-button.tsx
@@ -1,12 +1,33 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
+import { useTextStore } from '@/store/store';
+import { textType } from '@/types/types';
+import { useState } from 'react';
 
 export default function TextareaWithButton() {
+  const addText = useTextStore((state) => state.addText);
+  const [text, setText] = useState<string>('');
+  const onSubmit = () => {
+    if (text.trim()) {
+      const data: textType = {
+        id: '',
+        content: text,
+      };
+      addText(data);
+      setText(''); // Clear the textarea after adding
+    }
+  };
   return (
     <div className="grid w-full gap-2 font-neue">
-      <Textarea placeholder="Type your notes here." />
+      <Textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type your notes here."
+      />
       <div className="flex justify-end gap-2">
-        <Button className="bg-amber-100">Add Note</Button>
+        <Button className="bg-amber-100" onClick={onSubmit}>
+          Add Note
+        </Button>
         <Button variant={'secondary'} className="bg-amber-50">
           Clear Note
         </Button>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,17 @@
 import { textType } from '@/types/types';
-
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 type textState = {
   text: textType[];
   addText: (newText: textType) => void;
 };
 
-export const useTextStore = create<textState>((set) => ({
-  text: [],
-  addText: (newText) =>
-    set((state) => ({
-      text: [...state.text, { id: Date.now().toString(), content: newText.content }],
-    })),
-}));
+export const useTextStore = create<textState>()(
+  devtools((set) => ({
+    text: [],
+    addText: (newText) =>
+      set((state) => ({
+        text: [...state.text, { id: Date.now().toString(), content: newText.content }],
+      })),
+  }))
+);
